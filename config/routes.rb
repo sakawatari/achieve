3744c189bfbe5869a6a754e81215ed93dda81405
sakawatari <under.fild1930@gmail.com>
2016-11-21 05:39:57 +0000

    レファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -4,27 +4,21 @@ Rails.application.routes.draw do
       registrations: "users/registrations",
       omniauth_callbacks: "users/omniauth_callbacks"
     }
-
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
-
   resources :users, only: [:index, :show]
-
   resources :relationships, only: [:create, :destroy]
-
   resources :contacts, only: [:new, :create] do
     collection do
       post :confirm
     end
   end
-
   resources :blogs do
     resources :comments
     collection do
       post :confirm
     end
   end
-
   root 'top#index'
 end
