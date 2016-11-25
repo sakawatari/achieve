17437fa4cd566c05c7b22b2fa2bbd64b7e375d02
sakawatari <under.fild1930@gmail.com>
2016-11-25 05:53:40 +0000

    new-dive17-タスク機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
   has_many :followers, through: :reverse_relationships, source: :follower
+  has_many :tasks, dependent: :destroy
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :tasks
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
     devise_for :users, controllers: {
       registrations: "users/registrations",
