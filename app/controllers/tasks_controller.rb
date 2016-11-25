5d7ffe5ea3855b36a818ab9b172d5ff32b4106ac
sakawatari <under.fild1930@gmail.com>
2016-11-25 07:54:13 +0000

    new-dive17-タスク機能の追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -32,10 +32,8 @@ class TasksController < ApplicationController
     respond_to do |format|
       if @task.save
         format.html { redirect_to tasks_url, notice: 'タスクを作成しました' }
-        format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -46,10 +44,8 @@ class TasksController < ApplicationController
     respond_to do |format|
       if @task.update(task_params)
         format.html { redirect_to tasks_url, notice: 'タスクを編集しました' }
-        format.json { render :show, status: :ok, location: @task }
       else
         format.html { render :edit }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -60,7 +56,6 @@ class TasksController < ApplicationController
     @task.destroy
     respond_to do |format|
       format.html { redirect_to tasks_url, notice: 'タスクを削除しました' }
-      format.json { head :no_content }
     end
   end
 
