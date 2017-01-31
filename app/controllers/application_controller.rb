c0b6d019e7e2677028042043647662b17b6d9408
sakawatari <under.fild1930@gmail.com>
2017-01-31 07:18:18 +0000

    DIVE19-2 通知機能の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  # before_actionでdeviseのストロングパラメーターにnameカラムを追加するメソッドを実行します。
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :current_notifications, if: :signed_in?
   PERMISSIBLE_ATTRIBUTES = %i(name avatar avatar_cache)
+
+  def current_notifications
+    @notifications_count = Notification.where(user_id: current_user.id).where(read: false).count
+  end
 
   protected
 
@@ -13,7 +16,7 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:sign_up, keys: PERMISSIBLE_ATTRIBUTES)
       devise_parameter_sanitizer.permit(:account_update, keys: PERMISSIBLE_ATTRIBUTES)
     end
-    
+
     rescue_from CanCan::AccessDenied do |exception|
         redirect_to main_app.root_url, :alert => exception.message
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'notifications/index'
+
   resources :conversations do
     resources :messages
   end
