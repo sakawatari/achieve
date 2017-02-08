f9ff3df3bb87869da5bd04d679a59c2ffda1f7bf
sakawatari <under.fild1930@gmail.com>
2017-02-08 07:57:59 +0000

    change mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,8 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  # 
-  config.action_mailer.default_url_options = { host: 'ancient-reaches-18517.herokuapp.com' }
+  #
+  config.action_mailer.default_url_options = { host: '13.112.49.94' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
     {
