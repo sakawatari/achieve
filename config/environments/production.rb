cfbb5dbed982a7b4c56e54f822bc4c8d0475b6fd
sakawatari <under.fild1930@gmail.com>
2017-02-08 08:04:31 +0000

    change mailer domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
     {
      user_name: ENV['SENDGRID_USERNAME'],
      password: ENV['SENDGRID_PASSWORD'],
-     domain: "heroku.com",
+     domain: "13.112.49.94",
      address: "smtp.sendgrid.net",
      port: 587,
      authentication: :plain,
